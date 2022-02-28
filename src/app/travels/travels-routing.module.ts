0143531ce6dab12208aa7a4d75b898e42e912a3f
pankowskitomasz <pankowski.email@gmail.com>
2022-02-28 20:52:04 +0200

    routing

--- src/app/travels/travels-routing.module.ts
+++ src/app/travels/travels-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ComingsoonComponent } from './comingsoon/comingsoon.component';
+import { TravelsComponent } from './travels/travels.component';
 
 const routes: Routes = [
   {
-    path:"comingsoon",
-    component: ComingsoonComponent
+    path: "travels",
+    component: TravelsComponent
   }
 ];
 
@@ -13,4 +13,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class ComingsoonRoutingModule { }
+export class TravelsRoutingModule { }
---
+++ src/app/travels/travels/travels.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-travels',
+  templateUrl: './travels.component.html',
+  styleUrls: [
+    './travels.component.scss',
+    "../../sass/styles.scss"
+  ]
+})
+export class TravelsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
